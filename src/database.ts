c6b7a6a1a071c4caa6c1b519c206c1051451bd49
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2021-05-24 00:42:03 +0800

    try adding database

---
+++ src/database.ts
@@ -0,0 +1,124 @@
+/**
+ * Set up SQLite database
+ */
+
+import Database from 'better-sqlite3'
+
+export function openDb(dbName = 'database.db') {
+    return new Database(dbName, { verbose: console.log })
+}
+
+export function createSchema() {
+
+}
+
+/*
+
+1. ValidatorEpoch Table
+2. Validator Log Table
+2. PoolEpoch Table
+3. PoolValidator Table
+
+validatorepoch: validator, epoch
+validatorlog: (validator, epoch), timestamp
+poolepoch: (pool, epoch)
+poolvalidator: (pool, epoch), validator
+
+Validatorepoch one to many relationship with ValidatorLog
+PoolEpoch and ValidatorEpoch have many to many relationship
+ -- using junction table PoolValidator
+
+We also need a table to convert epoch credits to points
+
+getEpochInfo
+https://solana-labs.github.io/solana-web3.js/modules.html#epochinfo
+
+Epoch
+*/
+
+/*
+export function createTable(
+    tableName: string,
+    db: Database,
+    schema: Map) {
+    const createTableCommand = db.prepare(`CREATE TABLE IF NOT EXISTS ${tableName} (
+
+
+    )`)
+    console.log(createTableCommand)
+    db.transaction(() => {
+        createTableCommand.run();
+    })
+}
+*/
+
+export function createValidatorTable(
+    db: Database
+) {
+    console.log("Run command!")
+    const createValidatorTableCommand = db.prepare(
+        `CREATE TABLE IF NOT EXISTS validators (
+        votePubkey STRING,
+        epoch INTEGER, 
+        activatedStake INTEGER,
+        credits INTEGER,
+        epochVoteAccount BOOLEAN,
+        PRIMARY KEY (votePubkey, epoch)
+    );`)
+    db.transaction(() => {
+        createValidatorTableCommand.run();
+    })
+    console.log("Ran command!")
+}
+
+export function createValidatorLogTable(
+    db: Database
+) {
+    const command = db.prepare(
+        `CREATE TABLE IF NOT EXISTS validatorlogs (
+        votePubkey STRING,
+        epoch INTEGER, 
+        timestamp TIMESTAMP,
+        commission INTEGER,
+        credits INTEGER,
+        lastVote INTEGER,
+        PRIMARY KEY (votePubkey, epoch, timestamp)
+    );`)
+    db.transaction(() => { command.run() })
+    console.log("Created validatorlogs table!")
+}
+
+export function createPoolTable(
+    db: Database
+) {
+    console.log("Run command!")
+    const createValidatorTableCommand = db.prepare(
+        `CREATE TABLE IF NOT EXISTS pools (
+        stakerPubkey STRING,
+        epoch INTEGER, 
+        managerPubkey STRING,
+        depositAuthority,
+        totalStakeLamports,
+        poolTokenSupply,
+        PRIMARY KEY (stakerPubkey, epoch)
+    );`)
+    db.transaction(() => {
+        createValidatorTableCommand.run();
+    })
+    console.log("Ran command!")
+}
+
+
+export function createPoolValidatorTable(
+    db: Database
+
+) {
+    const command = db.prepare(`CREATE TABLE IF NOT EXISTS pool_validator (
+        stakerPubkey STRING,
+        epoch INTEGER, 
+        validatorPubkey STRING,
+        stakeInValidator INTEGER,
+        PRIMARY KEY (stakerPubkey, epoch, validatorPubkey)
+    )`)
+    db.transaction(() => command.run())
+}
---
+++ src/index.ts
@@ -0,0 +1,138 @@
+import { DatabaseDriver, MikroORM } from '@mikro-orm/core';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+
+
+import * as solanaWeb3 from '@solana/web3.js';
+import Database from 'better-sqlite3'
+import * as database from './database.js';
+
+async function getValidatorAccounts(
+    connection: solanaWeb3.Connection,
+    stakeProgramAddress: solanaWeb3.PublicKey,
+) {
+    return connection.getVoteAccounts("confirmed")
+
+
+    /*
+    .then((accounts) => {
+        /*
+        console.log("Current accounts: ")
+        accounts.current.map(
+            (account) => {
+                console.log(account.votePubkey)
+                console.log(account.lastVote)
+                console.log(account.commission)
+                console.log(account.epochCredits)
+
+                // EkLA4nA5jtM2t2FkNWo6XWAyvQyaJJUZoX5p7LMawoaz
+                //     79696696
+                //     10
+                //     [
+                //     [ 180, 55291399, 54982517 ],
+                //     [ 181, 55608535, 55291399 ],
+                //     [ 182, 55907113, 55608535 ],
+                //     [ 183, 56181346, 55907113 ],
+                //     [ 184, 56285458, 56181346 ]
+                //     ]
+            }
+        )
+
+        console.log("Delinquent accounts: ")
+        accounts.delinquent.map(
+            (account) => {
+                console.log(account.epochCredits)
+            }
+        )
+});
+        */
+}
+
+function populateValidatorTables(
+    accountsInfo: Array<solanaWeb3.VoteAccountInfo>,
+    epoch: number,
+    db: Database
+) {
+    /* Populate validator and validatorlog tables */
+
+    /* For each entry in epochCredits,
+       override the existing epochCredits
+     */
+    const insert = db.prepare(
+        `INSERT INTO validatorlogs VALUES (
+            @votePubkey,
+            @epoch,
+            @timestamp,
+            @commission,
+            @credits,
+            @lastVote
+            );`
+    );
+
+    const items = accountsInfo.filter((account) => {
+        account.epochCredits.map((a) => a[0]).includes(epoch)
+    }).map((account) => {
+        console.log(account);
+        return {
+            votePubkey: account.votePubkey,
+            epoch: epoch,
+            timestamp: new Date().toLocaleString(),
+            commission: account.commission,
+            credits: account.epochCredits.filter(x => x[0] == epoch)[1],
+            lastVote: account.lastVote,
+        }
+    })
+
+    const insertMany = db.transaction((logs) => {
+        for (const validatorLog of logs) insert.run(validatorLog);
+    })
+
+    console.log(items)
+    insertMany(items);
+}
+
+const connection = new solanaWeb3.Connection(
+    'https://api.mainnet-beta.solana.com/',
+    'confirmed',
+);
+
+const STAKE_PROGRAM_ADDR = new solanaWeb3.PublicKey(
+    'Stake11111111111111111111111111111111111111',
+);
+
+const db = database.openDb();
+const validatorTable = database.createValidatorTable(db);
+const validatorLogTable = database.createValidatorLogTable(db);
+main()
+
+async function main() {
+    const accounts = await getValidatorAccounts(connection, STAKE_PROGRAM_ADDR);
+    const activeAccounts = accounts.current;
+    const currentEpoch = (await connection.getEpochInfo("confirmed")).epoch;
+    populateValidatorTables(activeAccounts, currentEpoch, db)
+    const stmt = db.prepare('SELECT * from validatorlogs')
+    const logs = stmt.all();
+    console.log(logs);
+}
+
+
+/**
+ * 1. Connect to API mainnet
+ * 2. Poll for pubkey and epochCredits by calling `getVoteAccounts` and VoteAccountInfo
+ * 3. We update our SQL database
+ */
+// async function main() {
+//     const orm = await MikroORM.init({
+//         metadataProvider: TsMorphMetadataProvider,
+//         entities: ['./entities/**/*.js'],
+//         entitiesTs: ['./entities/**/*.ts'],
+//         dbName: 'stake-pool-dashboard-db',
+//         type: 'sqlite',
+//     });
+
+//     console.log(orm.em)
+
+// }
+
+
+// getValidatorAccounts(connection, STAKE_PROGRAM_ADDR);
+
